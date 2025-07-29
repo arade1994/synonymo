879d097433bddc7f99e5859e56d94239ff1134f1
Ante Rade <anterade99@gmail.com>
2025-07-29 14:18:04 +0200

    fix: fixed static export in build process

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,8 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "**/*.module.scss")],
   },
