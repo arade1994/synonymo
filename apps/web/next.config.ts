95cce832bd03fc71d60b32fb58ea5af289b7fdd3
Ante Rade <anterade99@gmail.com>
2025-07-29 14:06:17 +0200

    fix: fixed next config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
 import path from "path";
-import { withNetlify } from "@netlify/plugin-nextjs";
 
-const nextConfig: NextConfig = withNetlify({
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "**/*.module.scss")],
   },
-});
+};
 
 export default nextConfig;
